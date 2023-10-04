2f0177be54127ced67d54fa55333876ffcb6fc1a
Kabsa <kabsaabdi23@gmail.com>
2023-10-03 23:38:50 -0600

    Create kadane algo brute force

---
+++ kadane.py
@@ -0,0 +1,9 @@
+def bruteForce(nums):
+    maxSum = nums[0]
+
+    for i in range(len(nums)):
+        curSum = 0
+        for j in range(i, len(nums)):
+            curSum += nums[j]
+            maxSum = max(maxSum, curSum)
+    return maxSum
